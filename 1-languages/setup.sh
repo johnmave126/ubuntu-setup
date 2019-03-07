694b27d74fe6c2cbd40aef803610882ed13801e7
Shuhao <johnmave126@gmail.com>
2019-03-07 09:55:11 -0500

    Add haskell installation

--- 1-languages/setup.sh
+++ 1-languages/setup.sh
@@ -36,6 +36,12 @@ echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 tput rmcup
 
+# prepare haskell
+echo -e "\e[1m[languages] \e[0m\e[96mprepare haskell\e[0m"
+tput smcup
+sudo add-apt-repository -y ppa:hvr/ghc
+tput rmcup
+
 # install all
 echo -e "\e[1m[languages] \e[0m\e[96minstall everything\e[0m"
 tput smcup
@@ -50,7 +56,8 @@ sudo apt install -y nodejs \
                     libc++-7-dev libc++abi-7-dev \
                     libomp-7-dev \
                     mono-complete \
-                    oracle-java8-installer oracle-java8-set-default
+                    oracle-java8-installer oracle-java8-set-default \
+                    cabal-install-1.22 ghc-7.10.3
 tput rmcup
 
 # install rust
@@ -65,5 +72,11 @@ echo -e "\e[1m[languages] \e[0m\e[96minstall typescript\e[0m"
 tput smcup
 sudo npm install -g typescript
 tput rmcup
+
+# after install for haskell
+cat >> ~/.bashrc <<EOF
+export PATH='$HOME/.cabal/bin:/opt/cabal/1.22/bin:/opt/ghc/7.10.3/bin:$PATH'
+EOF
+export PATH=~/.cabal/bin:/opt/cabal/1.22/bin:/opt/ghc/7.10.3/bin:$PATH
 
 echo -e "\e[1m[languages] \e[0m\e[96mdone\e[0m"
