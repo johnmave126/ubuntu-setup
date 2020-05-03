cd206709f4ff01928be94c2931e54595249b44da
Shuhao Tan <johnmave126@gmail.com>
2020-05-03 17:06:43 -0400

    Bump programming language versions

--- 1-languages/setup.sh
+++ 1-languages/setup.sh
@@ -5,7 +5,7 @@
 # prepare nodejs
 echo -e "\e[1m[languages] \e[0m\e[96mprepare nodejs\e[0m"
 tput smcup
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 tput rmcup
 
 # prepare go
@@ -18,7 +18,7 @@ tput rmcup
 echo -e "\e[1m[languages] \e[0m\e[96mprepare clang\e[0m"
 tput smcup
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-sudo apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-7 main"
+sudo apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-10 main"
 tput rmcup
 
 # prepare mono
@@ -26,14 +26,6 @@ echo -e "\e[1m[languages] \e[0m\e[96mprepare mono\e[0m"
 tput smcup
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 sudo apt-add-repository "deb https://download.mono-project.com/repo/ubuntu stable-bionic main"
-tput rmcup
-
-# prepare java
-echo -e "\e[1m[languages] \e[0m\e[96mprepare mono\e[0m"
-tput smcup
-sudo add-apt-repository ppa:webupd8team/javaecho
-echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 tput rmcup
 
 # prepare haskell
@@ -48,16 +40,16 @@ tput smcup
 sudo apt update
 sudo apt install -y nodejs \
                     golang-go \
-                    libllvm-7-ocaml-dev libllvm7 llvm-7 llvm-7-dev llvm-7-doc llvm-7-examples llvm-7-runtime \
-                    clang-7 clang-tools-7 clang-7-doc libclang-common-7-dev libclang-7-dev libclang1-7 clang-format-7 python-clang-7 \
-                    libfuzzer-7-dev \
-                    lldb-7 \
-                    lld-7 \
-                    libc++-7-dev libc++abi-7-dev \
-                    libomp-7-dev \
+                    libllvm-10-ocaml-dev libllvm10 llvm-10 llvm-10-dev llvm-10-doc llvm-10-examples llvm-10-runtime \
+                    clang-10 clang-tools-10 clang-10-doc libclang-common-10-dev libclang-10-dev libclang1-10 clang-format-10 python-clang-10 \
+                    libfuzzer-10-dev \
+                    lldb-10 \
+                    lld-10 \
+                    libc++-10-dev libc++abi-10-dev \
+                    libomp-10-dev \
                     mono-complete \
-                    oracle-java8-installer oracle-java8-set-default \
-                    cabal-install-1.22 ghc-7.10.3
+                    openjdk-13-jdk \
+                    cabal-install-3.2 ghc-8.10.1
 tput rmcup
 
 # install stack
@@ -81,8 +73,8 @@ tput rmcup
 
 # after install for haskell
 cat >> ~/.bashrc <<EOF
-export PATH='$HOME/.cabal/bin:/opt/cabal/1.22/bin:/opt/ghc/7.10.3/bin:$PATH'
+export PATH='$HOME/.cabal/bin:/opt/cabal/3.2/bin:/opt/ghc/8.10.1/bin:$PATH'
 EOF
-export PATH=~/.cabal/bin:/opt/cabal/1.22/bin:/opt/ghc/7.10.3/bin:$PATH
+export PATH=~/.cabal/bin:/opt/cabal/3.2/bin:/opt/ghc/8.10.1/bin:$PATH
 
 echo -e "\e[1m[languages] \e[0m\e[96mdone\e[0m"
