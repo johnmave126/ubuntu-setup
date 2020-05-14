871cf86c63d772535909162d68a39404f9219c13
Youmu <johnmave126@gmail.com>
2020-05-14 11:08:51 -0400

    Insta

--- 2-tmux/setup.sh
+++ 2-tmux/setup.sh
@@ -9,4 +9,12 @@ sudo apt-get install -y tmux
 mkdir -p ~/.tmux
 echo -e "\e[1m[tmux] \e[0m\e[96mSet up tmux config\e[0m"
 cp .tmux.conf ~/.tmux.conf
+wget -O ~/.tmux/tmux-complete https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/master/completions/tmux
+echo ". ~/.tmux/tmux-complete" >>~/.bashrc
+
+# Set up plugins
+echo -e "\e[1m[tmux] \e[0m\e[96mSet up tmux plugins\e[0m"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+export TMUX_PLUGIN_MANAGER_PATH=$HOME/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/bin/install_plugins
 
